93b252d97af4939ce2877fb5540217f98ba43e1a
Rodrigo Costa <154683222+rodrigodevcosta@users.noreply.github.com>
2024-02-15 21:07:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-/*
-                                                            |
-+ ---------------------- Requisitos -------------------------- +
-|                             |                                |
-| - Cadastro clientes         |  - Interface principal         |
-| - Cadastro fornecedores     |  - Interface de cadastro       |
-| - Cadastro contas receber   |  - Uso de Herança              |
-| - Cadastro contas pagar     |  - Uso de encapsulamento       |
-| - Listar contas receber     |  - Uso de polimorfismo         |
-| - Listar contas pagar       |    (opcional)                  |
-|                             |                                |
-+ ------------------------------------------------------------ +
-*/
+
 
 #include <iostream>
 #include<InterfacePrincipal.h>
